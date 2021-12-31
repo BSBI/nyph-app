30ece8b34ec0997fff7ecc0ab8b59be7fb3e8530
Tom Humphrey <tomhumphrey@gmail.com>
2021-12-31 14:03:32 +0000

    again

--- public/app.js
+++ public/app.js
@@ -14912,7 +14912,7 @@
       value: function body() {
         // at this point the entire content of #body should be safe to replace
         var bodyEl = document.getElementById('body');
-        bodyEl.innerHTML = htmlContent + "<p>Version 1.0.3.1640953955</p>";
+        bodyEl.innerHTML = htmlContent + "<p>Version 1.0.3.1640959339</p>";
       }
     }]);
 
