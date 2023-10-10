ef3ee64a895e8a0d0b8583448385bb38afc58ace
Tom Humphrey <tomhumphrey+github@gmail.com>
2023-10-10 21:17:17 +0100

    rebuild

--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ import {MainController} from "./controllers/MainController";
 import {MainView} from "./views/MainView";
 import {HelpView} from "./views/HelpView";
 import {NyphLayout} from "./views/layout/NyphLayout";
+import mapboxglSupported from "@mapbox/mapbox-gl-supported";
+import mapboxgl from 'mapbox-gl';
 import './theme.scss';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import './app.css';
 import 'bsbi-app-framework-view/dist/css.css';
-import {StaticContentController, SurveyPickerController, Taxon} from "bsbi-app-framework";
+import 'dialog-polyfill/dist/dialog-polyfill.css';
+import {Model, StaticContentController, SurveyPickerController, Taxon} from "bsbi-app-framework";
 import {doubleClickIntercepted, NotFoundView, PatchedNavigo, SurveyPickerView} from "bsbi-app-framework-view";
 //import 'bootstrap';
 import Tab from "bootstrap/js/dist/tab";
@@ -20,18 +23,6 @@ import taxa from "https://staticdatabase.bsbi.org/js/taxonnames.mjs.php";
 
 //Taxon.setTaxa(taxa);
 Taxon.initialiseTaxa(taxa, "https://staticdatabase.bsbi.org/js/taxonnames.mjs.php");
-
-// work around Edge bug
-// if (!Promise.prototype.finally) {
-//     Promise.prototype.finally = function(callback) {
-//         return this.then(callback)
-//             .catch(callback);
-//     };
-// }
-
-// even though Rollup is bundling all your files together, errors and
-// logs will still point to your original source modules
-//console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
 
 /**
  * @type {ServiceWorkerRegistration}
@@ -55,7 +46,7 @@ if (navigator.serviceWorker) {
     // } else {
     navigator.serviceWorker.getRegistration(`/${pathPrefix}/serviceworker.mjs`).then((registration) => {
         haveExistingWorker = !!registration;
-        console.log(haveExistingWorker ? 'Have existing sevice worker' : 'No current service worker');
+        console.log(haveExistingWorker ? 'Have existing service worker' : 'No current service worker');
     }).then(() => {
         // Register the ServiceWorker limiting its action to those URLs starting
         // by 'controlled'. The scope is not a path but a prefix. First, it is
@@ -63,7 +54,6 @@ if (navigator.serviceWorker) {
         // controlled by testing it is a prefix of the request URL.
         navigator.serviceWorker.register(`/${pathPrefix}/serviceworker.mjs`, {
             type: 'module'
-            // scope: './controlled'
         }).then(reg => {
             serviceWorkerRegistration = reg;
 
@@ -89,6 +79,8 @@ if (navigator.serviceWorker) {
     });
     //}
 }
+
+Model.bsbiAppVersion = '__BSBI_APP_VERSION__';
 
 const app = new NyphApp;
 app.notFoundViewObject = new NotFoundView();
